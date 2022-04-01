71f94984de9435524b42762c7f4a367ab4e72b9f
Pico Dev <63173852+Pico-Dev@users.noreply.github.com>
2022-03-31 20:14:48 -0400

    Update archstall.sh

--- archstall.sh
+++ archstall.sh
@@ -84,6 +84,32 @@ EEOF
 		mount /dev/${vardisk}2 /mnt/boot
 		mkdir /mnt/etc
 		genfstab -U -p /mnt >> /mnt/etc/fstab
+		read -p "do you want to setup another disk (y/N) " an
+			case $an in
+				[yY] ) echo ok;
+					lsblk;
+					read -p "what disk: " diskan;
+					fdisk /dev/$diskan <<EEOF
+					g
+					n
+					
+					
+					
+					w
+EEOF;
+					cryptsetup luksFormat /dev/${diskan}1;
+					read -p "what do you want your disk to be called: " sdname;
+					cryptsetup open --type luks /dev/${diskan}1 $sdmane;
+					mkfs.ext4 /dev/mapper/$sdname;
+					dd if=/dev/urandom of=/root/keyfile bs=1024 count=4;
+					chmod 0400 /root/keyfile;
+					sudo cryptsetup luksAddKey /dev/${diskan}1;
+					echo "$sdname /dev/${diskan}1 /root/keyfile luks" | tee -a /etc/crypttab;
+					mkdir /mnt/$sdmane;
+					echo "/dev/mapper/$sdname /mnt/$sdmane ext4 defaults 0 2" | tee -a /etc/fstab;
+					break;;
+				* ) echo ok;
+					break;;
 	}
 	diskfom
 
