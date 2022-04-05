03227cc83b732fa2c3349cbffd2997f6d01e167d
Pico Dev <63173852+Pico-Dev@users.noreply.github.com>
2022-04-05 19:52:42 -0400

    Update extradisk.sh

--- extradisk.sh
+++ extradisk.sh
@@ -12,14 +12,14 @@ setupdisk(){
 EEOF
 	cryptsetup luksFormat /dev/${diskan}1
 	read -p "what do you want your disk to be called: " sdname
-	cryptsetup open --type luks /dev/${diskan}1 $sdnane
+	cryptsetup open --type luks /dev/${diskan}1 $sdname
 	mkfs.ext4 /dev/mapper/$sdname
 	dd if=/dev/urandom of=/root/keyfile bs=1024 count=4
 	chmod 0400 /root/keyfile
 	sudo cryptsetup luksAddKey /dev/${diskan}1
 	echo "$sdname /dev/${diskan}1 /root/keyfile luks" | tee -a /etc/crypttab
-	mkdir /mnt/$sdnane;
-	echo "/dev/mapper/$sdname /mnt/$sdmane ext4 defaults 0 2" | tee -a /etc/fstab
+	mkdir /mnt/$sdname;
+	echo "/dev/mapper/$sdname /mnt/$sdname ext4 defaults 0 2" | tee -a /etc/fstab
 }
 setupdisk
 
